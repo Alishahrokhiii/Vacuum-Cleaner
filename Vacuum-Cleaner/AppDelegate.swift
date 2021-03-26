18f3d6eca54b49e6a679179d1ba2814f791629df
Seyed Ali Shahrokhi <alishahrokhi@icloud.com>
2021-03-26 13:05:32 +0430

    Initial Commit

---
+++ Vacuum-Cleaner/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Vacuum-Cleaner
+//
+//  Created by Seyed Ali Shahrokhi on 1/6/1400 AP.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Vacuum-Cleaner/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Vacuum-Cleaner
+//
+//  Created by Seyed Ali Shahrokhi on 1/6/1400 AP.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
